2fe5b01a48dddd5beba2d96cb858a1b927feb45c
Joshua Tzucker <joshua.tz@gmail.com>
2021-12-05 22:41:54 -0800

    Fix: Add workaround, SDK resolution mismatch (#2)
    - SDK will only accept `250x128` for 2.13", but docs and app constant
    has it as `250x122`
    - Closes #2

--- app/src/main/java/com/joshuatz/nfceinkwriter/Constants.kt
+++ app/src/main/java/com/joshuatz/nfceinkwriter/Constants.kt
@@ -6,6 +6,7 @@ const val WaveShareUID = "WSDZ10m"
 
 // Order matches WS SDK Enum (except off by 1, due to zero-index)
 // @see https://www.waveshare.com/wiki/Android_SDK_for_NFC-Powered_e-Paper
+// @see https://github.com/RfidResearchGroup/proxmark3/blob/0d1f8ca957c0ae6f3039237889cdabe5921afe2d/client/src/cmdhfwaveshare.c#L81-L90
 val ScreenSizes = arrayOf(
     "2.13\"",
     "2.9\"",
@@ -19,7 +20,9 @@ val ScreenSizes = arrayOf(
 val DefaultScreenSize = ScreenSizes[1]
 
 val ScreenSizesInPixels = mapOf(
-    "2.13\"" to Pair(250, 122),
+    // The true resolution for 2.13" is 250x122, but there is a (likely) typo in the SDK
+    // @see https://github.com/joshuatz/nfc-epaper-writer/issues/2
+    "2.13\"" to Pair(250, 128),
     "2.9\"" to Pair(296, 128),
     "4.2\"" to Pair(400, 300),
     "7.5\"" to Pair(800, 480),
